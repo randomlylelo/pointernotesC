2531e6b35725784860d4c9a03afc3ba362426fe3
Leo Zhang <lzhang3@caltech.edu>
2022-03-22 16:17:00 -0700

    pointer arithmetic

--- main.cpp
+++ main.cpp
@@ -25,6 +25,13 @@ void pointerArithmetic() {
     int a = 10;
     int* p = &a;
 
+    printf("Address p %d\n", p);
+    printf("Value p %d\n", *p);
+    // +1, adds 4 to the memory address because an int has 4 bytes in memory.
+    // Bc add one is essentially move to next memory address.
+    printf("Size of integer is %d bytes\n", sizeof(int));
+    printf("Address p+1 %d\n", p+1);
+    printf("Value p+1 %d\n", *(p+1));
 }
 
 int main() {
